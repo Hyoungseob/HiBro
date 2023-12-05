7b6112cc14495219f37d95290d9bf3335cda75aa
cadi3149 <cadi3149@gmail.com>
2023-12-05 17:42:21 +0900

    상영관 삭제 테스트 구현

--- src/test/java/com/HiBro/entity/TheaterTest.java
+++ src/test/java/com/HiBro/entity/TheaterTest.java
@@ -26,7 +26,6 @@ TheaterRepository theaterRepository;
 			theater.setTheaterLocation("울산 삼산동" + i);
 			theater.setTheaterType("프리미엄" + i);
 
-
 			theaterRepository.save(theater);
 		}
 	}
@@ -38,6 +37,20 @@ TheaterRepository theaterRepository;
 		List<Theater> theaterList = theaterRepository.findByTheaterLocation("울산 삼산동");
 		for (Theater theater : theaterList) {
 			System.out.println(theater);
+		}
+	}
+
+	//삭제 할때 CASCADE : REMOVE 설정 하거나 고아객체로 만들어서 한번에 삭제 가능
+	//Seat 엔터티 ManyToOne
+	@Test
+	@DisplayName("상영관 삭제 테스트")
+	public void deleteTheater() {
+		this.createTheater();
+		Theater theater = theaterRepository.findByTheaterCode(1L);
+		theaterRepository.delete(theater);
+		List<Theater> theaterList = theaterRepository.findByTheaterLocation("울산 삼산동");
+		for (Theater theaters : theaterList) {
+			System.out.println(theaters);
 		}
 	}
 

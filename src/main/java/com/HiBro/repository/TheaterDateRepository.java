0ca65d13de9c6c5f3ec4b199f2e1b1ac202e7d21
cadi3149 <cadi3149@gmail.com>
2023-12-05 17:42:40 +0900

    상영관 스케쥴 리포지토리 생성

---
+++ src/main/java/com/HiBro/repository/TheaterDateRepository.java
@@ -0,0 +1,8 @@
+package com.HiBro.repository;
+
+
+import com.HiBro.entity.TheaterDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterDateRepository extends JpaRepository<TheaterDate, Long> {
+}

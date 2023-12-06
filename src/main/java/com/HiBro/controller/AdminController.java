768e3fa497c8902a095ffcab0850143aa9720602
ceasot <hawjdtn2@naver.com>
2023-12-06 15:34:25 +0900

    관리자 페이지 레이아웃 나눴음

---
+++ src/main/java/com/HiBro/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.HiBro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController{
+    @GetMapping("/admin")
+    public String admin(){
+        return "administrator/admin_inquiry";
+    }
+
+
+}
--- src/main/java/com/HiBro/dto/MemberDTO.java
+++ src/main/java/com/HiBro/dto/MemberDTO.java
@@ -22,6 +22,7 @@ public class MemberDTO{
     @NotEmpty(message = "이메일 입력은 필수입니다.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
+    private Integer point;
     private LocalDateTime regDate;
     private Role role;
 }

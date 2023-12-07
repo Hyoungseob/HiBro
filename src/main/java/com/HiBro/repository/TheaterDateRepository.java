c19c890d13589a967e66a83dc23334dcd32a6d04
cadi3149 <cadi3149@gmail.com>
2023-12-07 11:59:15 +0900

    엔터티 기본키 변수명 바꿔서 리포지토리도 바꿔줌

--- src/main/java/com/HiBro/repository/TheaterDateRepository.java
+++ src/main/java/com/HiBro/repository/TheaterDateRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface TheaterDateRepository extends JpaRepository<TheaterDate, Long> {
-	@Query("SELECT t FROM TheaterDate t WHERE t.theater.theaterCode = :theaterCode")
+	@Query("SELECT t FROM TheaterDate t WHERE t.theater.code = :theaterCode")
 	List<TheaterDate> findTheaterDateByTheaterCode(@Param("theaterCode") Long theaterCode);
 }

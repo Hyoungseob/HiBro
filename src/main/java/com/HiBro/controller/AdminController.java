4d117adf53da6c3f7ae20d07ff64580e1edffa65
ceasot <hawjdtn2@naver.com>
2023-12-07 17:51:18 +0900

    AdminController admin_member 부분 제작중 attribute는 만들어놨으니 페이지에서 받아서 처리하는거 만들어야함

--- src/main/java/com/HiBro/controller/AdminController.java
+++ src/main/java/com/HiBro/controller/AdminController.java
@@ -1,12 +1,29 @@
 package com.HiBro.controller;
 
+import com.HiBro.entity.Member;
+import com.HiBro.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.internal.util.Members;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
+@RequiredArgsConstructor
 public class AdminController{
+    MemberService memberService;
+
     @GetMapping("/admin")
-    public String admin(){
+    public String admin(Model model, Optional<Integer> page){
+        Pageable pageable = PageRequest.of(page.isPresent()? page.get():0,7);
+        List<Member> memberList = memberService.getMemberAll() ;
+        model.addAttribute("memberList",memberList);
+        model.addAttribute("maxPage",5);
         return "administrator/admin_member";
     }
 

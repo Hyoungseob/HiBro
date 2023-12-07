6683345ada7ada4f70cf1d6c6d734309c2999924
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-07 12:32:00 +0900

    예매 레파지토리 생성(검색 기능)

---
+++ src/main/java/com/HiBro/repository/TicketingRepository.java
@@ -0,0 +1,12 @@
+package com.HiBro.repository;
+
+import com.HiBro.entity.Ticketing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketingRepository extends JpaRepository<Ticketing, Long> {
+
+    List<Ticketing> findByMemberCodeOrderByTicketingCodeAsc(Long code);
+
+}

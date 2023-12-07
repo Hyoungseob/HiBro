44e5e5babe6ba8490e6592f6515650797c805dae
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-07 17:55:55 +0900

    영화 이미지 레파지토리 수정

--- src/main/java/com/HiBro/repository/MovieImgRepository.java
+++ src/main/java/com/HiBro/repository/MovieImgRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MovieImgRepository extends JpaRepository<MovieImg, Long> {
-   // List<MovieImg> findByMovieCodeOrderByMovieCodeAsc(Long code);
+   List<MovieImg> findByMovieCodeOrderByMovieCodeAsc(Long code);
 
     MovieImg findByMovieCodeAndImgType(Long code, ImgType imgType);
 

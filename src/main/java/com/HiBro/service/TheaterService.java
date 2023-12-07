285ea3a9907ff55b1ad6c2fd93f18f90d21a26c6
cadi3149 <cadi3149@gmail.com>
2023-12-07 12:00:12 +0900

    상영관 추가, 삭제를 TheaterService 에서 처리

--- src/main/java/com/HiBro/service/TheaterService.java
+++ src/main/java/com/HiBro/service/TheaterService.java
@@ -1,15 +1,34 @@
 package com.HiBro.service;
 
-import com.HiBro.repository.TheaterRepository;
+import com.HiBro.constant.Role;
+import com.HiBro.dto.TheaterDTO;
+import com.HiBro.entity.*;
+import com.HiBro.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TheaterService {
 	private final TheaterRepository theaterRepository;
+	private final MemberRepository memberRepository;
+
+	public Theater saveTheater(TheaterDTO theaterDTO, String id) {
+		Member member = memberRepository.findById(id);
+		if (member.getRole() == Role.ADMIN) {
+			Theater theater = Theater.createTheater(theaterDTO);
+			return theaterRepository.save(theater);
+		}
+		return null;
+	}
+
+	public void deleteTheater(TheaterDTO theaterDTO, String id) {
+		Member member = memberRepository.findById(id);
+		if (member.getRole() == Role.ADMIN) {
+			Theater theater = theaterRepository.findByCode(theaterDTO.getTheaterCode());
+			theaterRepository.delete(theater);
+		}
+	}
 }

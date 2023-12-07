16e56450226f1331b00c7b76a26d1c71a98910c8
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-07 19:28:07 +0900

    이유를 잘 모르겠는 쿼리 메서드 이름 수정

--- src/main/java/com/HiBro/repository/MovieRepository.java
+++ src/main/java/com/HiBro/repository/MovieRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    Movie findByMovieCode(Long movieCode);
+    Movie findByCode(Long code);
 
     List<Movie> findByMovieTitle(String movieTitle);
 }

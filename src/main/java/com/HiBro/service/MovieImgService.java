79972a86568c89a7592122295d6d1c860c069292
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-07 17:56:07 +0900

    영화 이미지 서비스 작성(삭제기능)

---
+++ src/main/java/com/HiBro/service/MovieImgService.java
@@ -0,0 +1,37 @@
+package com.HiBro.service;
+
+import com.HiBro.entity.MovieImg;
+import com.HiBro.repository.MovieImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MovieImgService {
+
+    MovieImgRepository movieImgRepository;
+
+    //이미지 유무 체크
+    public boolean checkImg(List<MovieImg> movieImgs){
+
+        if(movieImgs != null){
+            return true;
+        }
+        return false;
+    }
+
+    //이미지 일괄 삭제
+    public void deleteMovieImg(Long movieCode){
+        List<MovieImg> movieImgs = movieImgRepository.findByMovieCodeOrderByMovieCodeAsc(movieCode);
+
+        if(checkImg(movieImgs)){
+
+            for(MovieImg movieImg : movieImgs){
+                movieImgRepository.delete(movieImg);
+            }
+        }
+    }
+}

f47b750f477bbfa16052d25044e5546de9a66f57
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-07 18:14:56 +0900

    영화 서비스 작성완료 (생성 및 삭제 기능) 테스트 전

--- src/main/java/com/HiBro/service/MovieService.java
+++ src/main/java/com/HiBro/service/MovieService.java
@@ -20,6 +20,19 @@ public class MovieService {
 
     private final MovieRepository movieRepository;
 
+    private final MovieImgService movieImgService;
+
+    private final MovieVideoService movieVideoService;
+
+    //영화 DB저장
+    public Movie saveMovie(Movie movie){
+
+        return movieRepository.save(movie);
+
+    }
+
+
+    //영화 유무 체크
     public boolean checkMovie(Movie movie){
 
         if(movie != null){
@@ -29,14 +42,15 @@ public class MovieService {
 
     }
 
-    public Movie saveMovie(Long movieCode){
+    //특정 영화데이터 DB 삭제(영상,이미지 함께 삭제)
+    public void deleteMovie(Long movieCode){
 
         Movie movie = movieRepository.findByMovieCode(movieCode);
 
         if(checkMovie(movie)){
-            
+            movieRepository.delete(movie);
+            movieImgService.deleteMovieImg(movie.getCode());
+            movieVideoService.deleteMovieVideo(movie.getCode());
         }
-
     }
-
 }

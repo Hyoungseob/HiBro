23d51578ce59d662b95b03a05c906c2798c29adb
cadi3149 <cadi3149@gmail.com>
2023-12-08 17:46:26 +0900

    theater -> screen 바꾸고 새로운 theater 만듦

--- src/main/java/com/HiBro/service/TheaterService.java
+++ src/main/java/com/HiBro/service/TheaterService.java
@@ -1,27 +1,31 @@
 package com.HiBro.service;
 
-import com.HiBro.constant.Role;
-import com.HiBro.dto.TheaterDTO;
-import com.HiBro.entity.*;
+import com.HiBro.dto.*;
+import com.HiBro.entity.Theater;
 import com.HiBro.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import javax.transaction.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TheaterService {
 	private final TheaterRepository theaterRepository;
-	private final MemberRepository memberRepository;
+	private final ScreenRepository screenRepository;
+	private final ScreenService screenService;
 
 	public Theater saveTheater(TheaterDTO theaterDTO) {
 		Theater theater = Theater.createTheater(theaterDTO);
 		return theaterRepository.save(theater);
 	}
+	public void deleteTheater(TheaterDTO theaterDTO, ScreenDTO screenDTO) {
+		screenRepository.findByCode(screenDTO.getCode());
+		screenService.deleteScreen(screenDTO);
 
-	public void deleteTheater(TheaterDTO theaterDTO) {
 		Theater theater = theaterRepository.findByCode(theaterDTO.getCode());
 		theaterRepository.delete(theater);
 	}
+
 }

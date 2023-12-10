28317ea353705804b3712e1bedb14423d522512d
cadi3149 <cadi3149@gmail.com>
2023-12-10 12:40:56 +0900

    admin 브랜치 theater에 병합

--- src/main/java/com/HiBro/controller/AdminController.java
+++ src/main/java/com/HiBro/controller/AdminController.java
@@ -1,12 +1,38 @@
 package com.HiBro.controller;
 
+import com.HiBro.dto.MemberSearchDTO;
+import com.HiBro.entity.Member;
+import com.HiBro.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.internal.util.Members;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+	private final MemberService memberService;
+
 	@GetMapping("/admin")
-	public String admin() {
+	public String admin(MemberSearchDTO memberSearchDTO, Model model, Optional<Integer> page) {
+		Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 10);
+		Page<Member> memberList = memberService.getMemberAll(memberSearchDTO, pageable);
+		model.addAttribute("memberList", memberList);
+		model.addAttribute("maxPage", 5);
 		return "administrator/admin_member";
 	}
 
@@ -18,6 +44,12 @@ public class AdminController {
 	@GetMapping("/admin/inquiry")
 	public String inquiry() {
 		return "administrator/admin_inquiry";
+	}
+
+	@DeleteMapping("/admin/member/{member_code}")
+	public @ResponseBody ResponseEntity deleteMember(@PathVariable("member_code") Long memberCode, Principal principal) {
+		memberService.deleteMember(memberCode);
+		return new ResponseEntity<Long>(memberCode, HttpStatus.OK);
 	}
 
 	@GetMapping("/admin/theater")
---
+++ src/test/java/com/HiBro/repository/TicketingRepositoryTest.java
@@ -0,0 +1,85 @@
+package com.HiBro.repository;
+
+import com.HiBro.constant.AgeLimit;
+import com.HiBro.constant.Role;
+import com.HiBro.entity.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class TicketingRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
+    TheaterRepository theaterRepository;
+
+    @Autowired
+    TicketingRepository ticketingRepository;
+
+
+    Member 테스트용_멤버_데이터() {
+
+        Member member = new Member();
+
+        member.setEmail("dfads");
+        member.setId("sfdsdf");
+        member.setName("safdf");
+        member.setPoint(100);
+        member.setRole(Role.USER);
+        member.setPassword("sdfsf");
+        member.setRegDate(LocalDateTime.now());
+
+        return memberRepository.save(member);
+    }
+
+    Movie 테스트용_영화_데이터(){
+        Movie movie = new Movie();
+
+        movie.setMovieTitle("영화다");
+        movie.setGenre("개그");
+        movie.setSummary("몰라");
+        movie.setDirector("하정수");
+        movie.setActor("조범준");
+        movie.setAgeLimit(AgeLimit.ADULT);
+
+        return movieRepository.save(movie);
+
+    }
+
+    /*Screen 테스트용_상영관_데이터(){
+        Screen screen = new Screen();
+
+        screen.setTheaterLocation("울산 삼산");
+        screen.setTheaterImg("사진");
+        screen.setTheaterType("조범준의 집");
+
+        return theaterRepository.save(screen);
+    }
+
+    @Test
+    void 예매_생성및_조회기능_테스트(){
+
+        Ticketing ticketing = new Ticketing(테스트용_멤버_데이터());
+
+        ticketing.setMovie(테스트용_영화_데이터());
+        ticketing.setTheater(테스트용_상영관_데이터());
+
+        ticketingRepository.save(ticketing);
+
+        ticketingRepository.findByMemberCodeOrderByCodeAsc(ticketing.getMember().getCode());
+    }*/
+
+}

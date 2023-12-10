d4f3afea319cd55e0d78bea0409d12a115e90b7d
cadi3149 <cadi3149@gmail.com>
2023-12-10 12:24:42 +0900

    영화관 뷰 작업 시작

--- src/main/java/com/HiBro/controller/AdminController.java
+++ src/main/java/com/HiBro/controller/AdminController.java
@@ -4,19 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController{
-    @GetMapping("/admin")
-    public String admin(){
-        return "administrator/admin_member";
-    }
+public class AdminController {
+	@GetMapping("/admin")
+	public String admin() {
+		return "administrator/admin_member";
+	}
 
-    @GetMapping("/admin/point")
-    public String point(){
-        return "administrator/admin_point";
-    }
+	@GetMapping("/admin/point")
+	public String point() {
+		return "administrator/admin_point";
+	}
 
-    @GetMapping("/admin/inquiry")
-    public String inquiry(){
-        return "administrator/admin_inquiry";
-    }
+	@GetMapping("/admin/inquiry")
+	public String inquiry() {
+		return "administrator/admin_inquiry";
+	}
+
+	@GetMapping("/admin/theater")
+	public String theater() {
+		return "administrator/admin_theater";
+	}
 }

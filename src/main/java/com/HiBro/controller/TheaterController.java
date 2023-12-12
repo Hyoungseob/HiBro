e2650d0b933263a09edbbcc022e7994e9da34672
cadi3149 <cadi3149@gmail.com>
2023-12-12 17:24:24 +0900

    예매 뷰에다 데이터 뿌려줌

--- src/main/java/com/HiBro/controller/TheaterController.java
+++ src/main/java/com/HiBro/controller/TheaterController.java
@@ -1,13 +1,23 @@
 package com.HiBro.controller;
 
+import com.HiBro.entity.Theater;
+import com.HiBro.repository.TheaterRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class TheaterController {
-	@GetMapping("/theater")
-	public String theater() {
+import java.util.List;
 
+@Controller
+@RequiredArgsConstructor
+public class TheaterController {
+
+	private final TheaterRepository theaterRepository;
+	@GetMapping("/theater")
+	public String theater(Model model) {
+		List<Theater> theaterList = theaterRepository.findAll();
+		model.addAttribute("theaterList", theaterList);
 		return "ticketing";
 	}
 }

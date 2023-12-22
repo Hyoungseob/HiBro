6a88957cf6c24a5f0c424c71879601f8ae76ded6
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-22 09:17:52 +0900

    커스텀한 쿼리 리팩토링으로 인한 GetMapping 부분 수정

--- src/main/java/com/HiBro/repository/MovieChartRepositoryImpl.java
+++ src/main/java/com/HiBro/repository/MovieChartRepositoryImpl.java
@@ -5,35 +5,68 @@ import com.HiBro.dto.MovieChartFormDTO;
 import com.HiBro.dto.QMovieChartFormDTO;
 import com.HiBro.entity.QMovie;
 import com.HiBro.entity.QMovieImg;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
-public class MovieChartRepositoryImpl implements MovieChartRepository{
+public class MovieChartRepositoryImpl implements MovieChartRepository {
 
     private JPAQueryFactory queryFactory;
 
-    public MovieChartRepositoryImpl(EntityManager em){
+    public MovieChartRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public List<MovieChartFormDTO> getMovieChartFormDTOList() {
+    //검색 조건 유무를 판단해서 where절 조건 추가
+    private BooleanExpression whereArg(QMovie qMovie, String keyword){
+        if(keyword=="" || keyword==null){
+            return null;
+        }
+        return qMovie.movieTitle.eq(keyword);
+    }
+
+    /**@쿼리문 where문에 whereArg()메서드 적용 전 사용 했던 추가 메서드
+    public List<MovieChartFormDTO> getSearchList() {
         QMovie movie = QMovie.movie;
         QMovieImg movieImg = QMovieImg.movieImg;
 
-        List<MovieChartFormDTO> MovieChartFormDTOList = queryFactory.select(
-                new QMovieChartFormDTO(
-                        movie.code,
-                        movie.movieTitle,
-                        movie.premiereDate,
-                        movieImg.imgUrl)
-        )
+        List<MovieChartFormDTO> movieChartFormDTOList = queryFactory.select(
+                        new QMovieChartFormDTO(
+                                movie.code,
+                                movie.movieTitle,
+                                movie.premiereDate,
+                                movieImg.imgUrl)
+                )
                 .from(movieImg)
                 .join(movieImg.movie, movie)
                 .where(movieImg.imgType.eq(ImgType.POSTER))
                 .fetch();
 
-        return MovieChartFormDTOList;
+        return movieChartFormDTOList;
+    }*/
+
+    //QueryDSL을 활용한 쿼리 커스텀
+    public List<MovieChartFormDTO> getMovieChartFormDTOList(String keyword){
+        QMovie movie = QMovie.movie;
+        QMovieImg movieImg = QMovieImg.movieImg;
+
+        List<MovieChartFormDTO> searchList = queryFactory.select(
+                new QMovieChartFormDTO(
+                        movie.code,
+                        movie.movieTitle,
+                        movie.premiereDate,
+                        movieImg.imgUrl)
+                )
+                .from(movieImg)
+                .join(movieImg.movie, movie)
+                .where(movieImg.imgType.eq(ImgType.POSTER), whereArg(movie, keyword))
+                .fetch();
+
+        return searchList;
     }
+
+
+
 }

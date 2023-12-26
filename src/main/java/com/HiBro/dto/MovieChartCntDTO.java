55dd15126cbdf8b8979d9f2f4bd87b81945a4c26
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-26 17:51:56 +0900

    더보기 로직 처리를 위한 DTO생성

---
+++ src/main/java/com/HiBro/dto/MovieChartCntDTO.java
@@ -0,0 +1,15 @@
+package com.HiBro.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Optional;
+
+@Getter @Setter
+public class MovieChartCntDTO {
+
+    private Integer movieChartCnt;
+    private String searchMovieTitle;
+
+}

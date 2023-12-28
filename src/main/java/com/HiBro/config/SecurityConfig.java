d1ff8a2e30c00a30c4ef5b8f5bb4df3e2a0be2ff
Hyoungseob <hwangyoungseob5092@gmail.com>
2023-12-28 10:31:56 +0900

    시큐리티 적용을 위한 무비 차트 경로 허용설정

--- src/main/java/com/HiBro/config/SecurityConfig.java
+++ src/main/java/com/HiBro/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
 				.logoutSuccessUrl("/");
 
 		http.authorizeRequests()
-				.mvcMatchers("/css/**", "/js/**", "/img/**", "/video/**", "/screen/**").permitAll()
+				.mvcMatchers("/css/**", "/js/**", "/img/**", "/video/**", "/screen/**", "/movieChart/**").permitAll()
 				.mvcMatchers("/", "/member/**").permitAll()
 				.mvcMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().authenticated();
